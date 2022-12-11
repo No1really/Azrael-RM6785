12a43f55fd693024f9473e86425dd8152c771cf8
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2022-12-12 00:59:18 +0530

    Update User Info

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G
 export ARCH=arm64
-export KBUILD_BUILD_HOST=AbzRaider&Mr.Mnml
-export KBUILD_BUILD_USER="AbzRaider&Mr.Mnml"
+export KBUILD_BUILD_HOST=RM6785•DEV
+export KBUILD_BUILD_USER="AbzRaider"
 git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git clang
 
 [ -d "out" ] && rm -rf AnyKernel && rm -rf out || mkdir -p out
