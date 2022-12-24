a4b4224d91dc875d04c1e34b396b091605dbd517
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2022-12-24 19:12:55 +0530

    drivers : thermal : Turn Off MD Throttle Cooler V2

--- drivers/misc/mediatek/thermal/mt6785/inc/mtk_cooler_setting.h
+++ drivers/misc/mediatek/thermal/mt6785/inc/mtk_cooler_setting.h
@@ -20,11 +20,11 @@
  */
  /* mtk_cooler_mutt.c */
 /* 1: turn on MD throttle V2 cooler; 0: turn off */
-#define FEATURE_MUTT_V2			(1)
+#define FEATURE_MUTT_V2			(0)
 
 /* mtk_cooler_mutt.c */
 /* 1: turn on MD Thermal Warning Notification; 0: turn off */
-#define FEATURE_THERMAL_DIAG		(1)
+#define FEATURE_THERMAL_DIAG		(0)
 
 /* mtk_cooler_mutt.c */
 /* 1: turn on adaptive MD throttle cooler; 0: turn off  */
